78b77b77cf93ae4da5ca9af5aa1f632ca9d3aa7c
chrissyast <cjastles@gmail.com>
2020-08-28 16:59:26 +0100

    get env locally, fix cors origin

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ var cors = require('cors')
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 const dotenv = require('dotenv')
-dotenv.config()
+if (process.env.NODE_ENV === "development") {
+  const envPath = '../.env'
+}
+dotenv.config({path: envPath})
 const port = process.env.PORT || 4000
 const backend = process.env.BACKEND_SERVER || 'http://localhost:3001'
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGINS);
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
